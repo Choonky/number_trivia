211159f339480e3f61e8ef8e279e86af549521d0
JC Sindo <digitaljoni@gmail.com>
2020-11-11 23:16:09 +0800

    step_2

--- lib/pages/trivia_page.dart
+++ lib/pages/trivia_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:number_trivia/widgets/trivia_description.dart';
 import 'package:number_trivia/widgets/trivia_title.dart';
+import 'dart:math';
 
-class TriviaPage extends StatelessWidget {
+class TriviaPage extends StatefulWidget {
   const TriviaPage({Key key}) : super(key: key);
+
+  @override
+  _TriviaPageState createState() => _TriviaPageState();
+}
+
+class _TriviaPageState extends State<TriviaPage> {
+  final Random random = Random();
+  int randomNumber = 4200;
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +23,17 @@ class TriviaPage extends StatelessWidget {
       backgroundColor: Colors.blue[100],
       body: Column(
         children: [
-          TriviaTitle('4200'),
+          TriviaTitle('$randomNumber'),
           TriviaDescription(
             '4200 is the number of standard size of a legion in the Roman Republic',
           ),
           ElevatedButton(
-            onPressed: null,
+            onPressed: () {
+              print(randomNumber);
+              setState(() {
+                randomNumber = random.nextInt(9999);
+              });
+            },
             child: Text('Random Trivia'),
           ),
         ],
